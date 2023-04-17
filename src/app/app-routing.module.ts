c07f5e74e67ffae96a200a7f5ea04fd28f674397
Ton DeVeros <TonDeveros@gmail.com>
2023-04-17 18:34:52 -0300

    1st commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EnterPageComponent } from './enter-page/enter-page.component';
+import { RegisterComponent } from './register/register.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',            component: LoginComponent },
+  { path: 'enter-page', component: EnterPageComponent},
+  { path: 'schedule',      component: ScheduleComponent},
+  { path: 'register',      component: RegisterComponent},
+  { path: 'login-page',      component: RegisterComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
